19a100c07b93fef964c4725804ba8d87de61347c
Sergey Spiridonov <sena@s73.work>
2025-06-24 17:37:50 +0200

    pass address from command line

--- app/usender/main.cpp
+++ app/usender/main.cpp
@@ -1,9 +1,9 @@
+#include "uzel/addr.h"
 #include "uzel/msg.h"
 #include <uzel/netclient.h>
 
 #include <iostream>
-#include <memory>
-#include <stdexcept>
+#include <utility>
 
 /** simple app that will send a message to echo every 2 seconds */
 
@@ -15,10 +15,12 @@ const int send_s = 2;
 class NetPrinter : public NetClient
 {
 public:
-  NetPrinter(boost::asio::io_context& io_context)
-    : NetClient(io_context, 32300),  m_sendTimer(io_context, io::chrono::seconds(send_s))
+  explicit NetPrinter(boost::asio::io_context& io_context, uzel::Addr addr)
+    : NetClient(io_context, 32300),  m_sendTimer(io_context, io::chrono::seconds(send_s)), m_addrto(std::move(addr))
   {
-    s_authSuccess.connect([&](){std::cout << "auth is fired, calling sendMsg()" << std::endl; sendMsg();});
+    s_authSuccess.connect([&](){
+      std::cout << "auth is fired, calling sendMsg()" << std::endl;
+      sendMsg();});
   }
 
   void dispatch(uzel::Msg &msg)
@@ -28,16 +30,17 @@ public:
 
   void sendMsg()
   {
-    std::cout << "sendMsg() is called" << std::endl;
+    std::cout << "sendMsg() is called at " << __FILE_NAME__ << ": " << __LINE__ << std::endl;
     m_sendTimer.expires_after(io::chrono::seconds(send_s));
     m_sendTimer.async_wait([this](const boost::system::error_code&  /*ec*/){sendMsg();});
 
     uzel::Msg::ptree msgbody;
     msgbody.put("payload", "ping");
-    send(uzel::Msg("uecho","liver", std::move(msgbody)));
+    send(uzel::Msg(m_addrto, std::move(msgbody)));
   }
 private:
   boost::asio::steady_timer m_sendTimer;
+  uzel::Addr m_addrto;
 };
 
 
@@ -46,9 +49,14 @@ int main(int argc, char* argv[])
   try
   {
     boost::asio::io_context io_context;
+    if(argc < 2)
+    {
+      std::cerr << "Usage : \n  usender <appname>[@<hostname>]" << std::endl;
+      ::exit(150);
+    }
 
       //NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    NetPrinter printerapp(io_context);
+    NetPrinter printerapp(io_context, uzel::Addr(argv[1]));
     io_context.run();
   }
   catch (const std::exception& ex)
