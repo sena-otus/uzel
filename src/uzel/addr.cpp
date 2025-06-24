a4e3d408927c1bce98d4b1b75080773d28e2085f
Sergey Spiridonov <sena@s73.work>
2025-06-24 17:33:50 +0200

    initialize address from one string

--- src/uzel/addr.cpp
+++ src/uzel/addr.cpp
@@ -1,9 +1,22 @@
 #include "addr.h"
 
+#include <string>
+#include <utility>
 
 namespace uzel {
   Addr::Addr(std::string appname, std::string nodename)
     : m_appname(std::move(appname)), m_nodename(std::move(nodename))
+  {
+  }
+
+  Addr::Addr(const std::string &fulladdr, const size_t atpos)
+    : Addr(fulladdr.substr(0, atpos), (atpos == std::string::npos) ? "" : fulladdr.substr(atpos+1))
+  {
+  }
+
+
+  Addr::Addr(const std::string &fulladdr)
+    : Addr(fulladdr, fulladdr.find('@'))
   {
   }
 }
--- src/uzel/addr.h
+++ src/uzel/addr.h
@@ -10,9 +10,14 @@ namespace uzel
   public:
     Addr() = default;
     Addr(std::string appname, std::string nodename);
+      /** Same as above, but address in one string
+       * @param fulladdr <appname>[@<nodename>]
+       */
+    Addr(const std::string &fulladdr);
     [[nodiscard]] std::string app() const {return m_appname;}
     [[nodiscard]] std::string node() const {return m_nodename;}
   private:
+    Addr(const std::string &fulladdr, size_t atpos);
     std::string m_appname;
     std::string m_nodename;
   };
