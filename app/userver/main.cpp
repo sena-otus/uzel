531e4cc6422195d45d23de9c8c9b71510cd2b545
sena@s73.work <Sergey>
2025-06-29 04:33:53 +0200

    logging with flush

--- app/userver/main.cpp
+++ app/userver/main.cpp
@@ -2,12 +2,40 @@
 
 #include <uzel/dbg.h>
 
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/expressions.hpp>
 
 const int generic_errorcode = 200;
+
+void init_logging_with_flush()
+{
+  namespace logging = boost::log;
+  namespace expr = boost::log::expressions;
+  namespace attrs = boost::log::attributes;
+
+  boost::log::add_console_log(
+    std::clog,
+    boost::log::keywords::auto_flush = true,
+    boost::log::keywords::format = (
+      expr::stream
+      << "[" << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f") << "] "
+      << "[" << expr::attr<attrs::current_thread_id::value_type>("ThreadID") << "] "
+      << "[" << logging::trivial::severity << "] "
+      << expr::smessage
+                                    )
+                              );
+
+    boost::log::add_common_attributes();
+}
 
 
 int main(int argc, char* argv[])
 {
+//  init_logging_with_flush();
+
   try
   {
     boost::asio::io_context io_context;
