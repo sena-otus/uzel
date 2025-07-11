cc90eeacb0b1d771a6ac02c31010aba02988ffbe
Sergey Spiridonov <sena@s73.work>
2025-07-11 15:37:51 +0200

    more remote class to separate file

--- app/userver/netserver.cpp
+++ app/userver/netserver.cpp
@@ -16,65 +16,6 @@ const int delay_reconnect_s = 10;
 const int resolver_threads = 5;
 
 
-remote::remote(std::string nodename)
-  : m_node(std::move(nodename))
-{
-}
-
-
-void remote::addSession(session::shr_t ss)
-{
-  bool wasEmpty = m_session.empty();
-  m_session.push_back(ss);
-  BOOST_LOG_TRIVIAL(debug) << "there are now " << m_session.size() << " session(s) for " << m_node;
-  ss->s_send_error.connect([&]() { on_session_error(ss);});
-  ss->s_receive_error.connect([&]() { on_session_error(ss);});
-  if(wasEmpty)
-  { // first session
-    ss->takeOverMessages(m_outHighQueue);
-  } else {
-    // secondary connection or new connection
-    // * secondary connection will have the same remote UUID
-    // * new connection will have new remote UUID, that could be
-    // ** remote app was restarted
-    // ** remote app is a duplicate
-  }
-}
-
-bool remote::connected() const
-{
-  return !m_session.empty();
-}
-
-void remote::on_session_error(session::shr_t ss)
-{
-  BOOST_LOG_TRIVIAL(debug) << "error on session " << ss << ", excluding it from list";
-  for(auto ssi = m_session.begin(); ssi != m_session.end(); ++ssi)
-  {
-    if(*ssi == ss)
-    {
-      m_session.erase(ssi);
-      BOOST_LOG_TRIVIAL(debug) << "excluded, new list size is " << m_session.size();
-      return;
-    }
-  }
-}
-
-
-void remote::send(const uzel::Msg &msg)
-{
-  if(m_session.empty())
-  {
-      // no connection
-    m_outHighQueue.emplace(msg.str());
-  }
-  else {
-      // the very last session is the highest priority!
-    m_session.back()->putOutQueue(msg);
-  }
-}
-
-
 
 NetServer::NetServer(io::io_context& io_context, unsigned short port)
   : m_acceptor(io_context, tcp::endpoint(tcp::v6(), port)),  m_aresolver{resolver_threads, io_context}, m_iocontext(io_context)
--- app/userver/netserver.h
+++ app/userver/netserver.h
@@ -1,35 +1,13 @@
 #pragma once
 
+#include "remote.h"
+
 #include <uzel/aresolver.h>
 #include <uzel/session.h>
 #include <boost/asio.hpp>
-#include <list>
 #include <map>
 
 class session;
-
-/** incapsulates logical channel to one remote */
-class remote
-{
-public:
-  explicit remote(std::string name);
-
-  remote &operator=(const remote &) = delete;
-  remote(const remote &other) = delete;
-  remote(remote &&) = default;
-  remote &operator=(remote &&) = default;
-
-  void addSession(session::shr_t);
-  void send(const uzel::Msg &msg);
-  [[nodiscard]] bool connected() const;
-private:
-  void on_session_error(session::shr_t ss);
-
-  std::string m_node; ///! remote node name
-  std::list<session::shr_t> m_session; ///! keeps list of tcp sessions
-  std::queue<std::string> m_outHighQueue; ///! high prirority outgoing queue
-  std::queue<std::string> m_outLowQueue; ///! low priority outgoing queue
-};
 
 
 /** @brief tcp server */
