04c534fcecbb9fa81de7808b48789eda0728ac3c
sena@s73.work <Sergey>
2025-08-03 17:48:24 +0200

    create ptree translator for uzel::Priority

--- src/uzel/session.h
+++ src/uzel/session.h
@@ -11,6 +11,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/asio.hpp>
 
+#include <charconv>
 #include <unordered_map>
 #include <unordered_set>
 #include <deque>
@@ -176,4 +177,42 @@ inline std::istream& operator>>(std::istream& is, uzel::Priority& pr)
     pr = *rez;
   }
   return is;
+}
+
+/** translator for property tree */
+
+template<typename EnumClass>
+struct BEnumIntTranslator {
+  using internal_type = std::string;
+  using external_type = EnumClass;
+
+  boost::optional<external_type> get_value(const internal_type& str) {
+    int val{};
+    auto [ptr, ec] = std::from_chars(str.data(), str.data() + str.size(), val);
+    if (ec != std::errc()) {
+      return boost::optional<external_type>(boost::none);
+    }
+    auto maybe_val = EnumClass::_from_integral_nothrow(val);
+    if(maybe_val) {
+      return boost::optional<external_type>(*maybe_val);
+    }
+    return boost::optional<external_type>(boost::none);
+  }
+
+  boost::optional<internal_type> put_value(const external_type& value) {
+    return boost::optional<internal_type>(value._to_integral());
+  }
+};
+
+
+using PriorityTranslator = BEnumIntTranslator<uzel::Priority>;
+
+// Register translators
+namespace boost {
+  namespace property_tree {
+    template<typename Ch, typename Traits, typename Alloc>
+    struct translator_between<std::basic_string<Ch, Traits, Alloc>, uzel::Priority> {
+      using type = PriorityTranslator;
+    };
+  }
 }
