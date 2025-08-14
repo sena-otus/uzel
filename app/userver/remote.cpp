376043d8cd2c84d2267311901ff9ac579bf15034
sena@s73.work <Sergey>
2025-08-15 01:44:05 +0200

    indentation

--- app/userver/remote.cpp
+++ app/userver/remote.cpp
@@ -9,60 +9,60 @@ namespace uzel
 {
 
 
-remote::remote(NetAppContext::shr_t netctx, std::string nodename)
-  : m_netctx(std::move(netctx)), m_node(std::move(nodename))
-{
-}
+  remote::remote(NetAppContext::shr_t netctx, std::string nodename)
+    : m_netctx(std::move(netctx)), m_node(std::move(nodename))
+  {
+  }
 
 
-void remote::addSession(session::shr_t ss)
-{
-  ss->s_closed.connect([&](session::shr_t ss) { onSessionClosed(ss);});
-  m_priorityH = m_netctx->dispatcher()->registerHandler("priority", [this](const Msg &msg){ handlePriorityMsg(msg); });
+  void remote::addSession(session::shr_t ss)
+  {
+    ss->s_closed.connect([&](session::shr_t ss) { onSessionClosed(ss);});
+    m_priorityH = m_netctx->dispatcher()->registerHandler("priority", [this](const Msg &msg){ handlePriorityMsg(msg); });
 
-    // who is the boss?
-  if(ss->peerNode() > uzel::UConfigS::getUConfig().nodeName()) {
-    BOOST_LOG_TRIVIAL(debug) << "he is the boss, because '"
-                             << uzel::UConfigS::getUConfig().nodeName()
-                             << "' < '"<< ss->peerNode() << "'";
-    m_sessionWaitForRemote.emplace(ss);
-    BOOST_LOG_TRIVIAL(debug) << "there are now " << m_sessionWaitForRemote.size()
-                             << " session(s) for " << m_node << " waiting for decision";
+      // who is the boss?
+    if(ss->peerNode() > uzel::UConfigS::getUConfig().nodeName()) {
+      BOOST_LOG_TRIVIAL(debug) << "he is the boss, because '"
+                               << uzel::UConfigS::getUConfig().nodeName()
+                               << "' < '"<< ss->peerNode() << "'";
+      m_sessionWaitForRemote.emplace(ss);
+      BOOST_LOG_TRIVIAL(debug) << "there are now " << m_sessionWaitForRemote.size()
+                               << " session(s) for " << m_node << " waiting for decision";
 
-      //    auto iprio = ss->msg1().pbody().get<int8_t>("priority");
-      //    ss->setPriority(uzel::Priority::_from_integral(iprio));
-  } else {
-    BOOST_LOG_TRIVIAL(debug) << "we are the boss, because '"
-                             << uzel::UConfigS::getUConfig().nodeName()
-                             << "' > '"<< ss->peerNode() << "'";
-    if(!m_sessionH)
-    {
-      BOOST_LOG_TRIVIAL(debug) << "got high priority connection with '" << m_node
-                               << "' = " << ss;
-      m_sessionH = ss;
-      ss->setPriority(uzel::Priority::high);
-        // it is not necessary both sides to have same priority on the channel
-        // but let's do it
-      uzel::Msg::ptree body{};
-      body.add("priority", static_cast<uzel::Priority>(uzel::Priority::high));
-      ss->putOutQueue(std::make_shared<uzel::Msg>(uzel::Addr(), "priority", std::move(body)));
-      ss->takeOverMessages(m_outHighQueue);
-    } else if(!m_sessionL) {
-      BOOST_LOG_TRIVIAL(debug) << "got low priority connection with '" << m_node
-                               << "' = " << ss;
-      ss->setPriority(uzel::Priority::low);
-      m_sessionL = ss;
-      uzel::Msg::ptree body{};
-      body.add("priority", static_cast<uzel::Priority>(uzel::Priority::low));
-      ss->putOutQueue(std::make_shared<uzel::Msg>(uzel::Addr(), "priority", std::move(body)));
-      ss->takeOverMessages(m_outLowQueue);
+        //    auto iprio = ss->msg1().pbody().get<int8_t>("priority");
+        //    ss->setPriority(uzel::Priority::_from_integral(iprio));
     } else {
-      BOOST_LOG_TRIVIAL(debug) << "got duplicated connection with '" << m_node
-                               << "' = " << ss << ", will be closed";
-      ss->gracefullClose("duplicated connection");
+      BOOST_LOG_TRIVIAL(debug) << "we are the boss, because '"
+                               << uzel::UConfigS::getUConfig().nodeName()
+                               << "' > '"<< ss->peerNode() << "'";
+      if(!m_sessionH)
+      {
+        BOOST_LOG_TRIVIAL(debug) << "got high priority connection with '" << m_node
+                                 << "' = " << ss;
+        m_sessionH = ss;
+        ss->setPriority(uzel::Priority::high);
+          // it is not necessary both sides to have same priority on the channel
+          // but let's do it
+        uzel::Msg::ptree body{};
+        body.add("priority", static_cast<uzel::Priority>(uzel::Priority::high));
+        ss->putOutQueue(std::make_shared<uzel::Msg>(uzel::Addr(), "priority", std::move(body)));
+        ss->takeOverMessages(m_outHighQueue);
+      } else if(!m_sessionL) {
+        BOOST_LOG_TRIVIAL(debug) << "got low priority connection with '" << m_node
+                                 << "' = " << ss;
+        ss->setPriority(uzel::Priority::low);
+        m_sessionL = ss;
+        uzel::Msg::ptree body{};
+        body.add("priority", static_cast<uzel::Priority>(uzel::Priority::low));
+        ss->putOutQueue(std::make_shared<uzel::Msg>(uzel::Addr(), "priority", std::move(body)));
+        ss->takeOverMessages(m_outLowQueue);
+      } else {
+        BOOST_LOG_TRIVIAL(debug) << "got duplicated connection with '" << m_node
+                                 << "' = " << ss << ", will be closed";
+        ss->gracefullClose("duplicated connection");
+      }
     }
   }
-}
 
   void remote::handlePriorityMsg(const Msg &msg)
   {
@@ -116,10 +116,10 @@ void remote::addSession(session::shr_t ss)
   }
 
 
-bool remote::connected() const
-{
-  return m_sessionH && m_sessionL;
-}
+  bool remote::connected() const
+  {
+    return m_sessionH && m_sessionL;
+  }
 
   size_t remote::sessionCount() const
   {
@@ -127,32 +127,32 @@ bool remote::connected() const
   }
 
 
-void remote::onSessionClosed(session::shr_t ss)
-{
-  BOOST_LOG_TRIVIAL(debug) << "error on session " << ss << ", excluding it from list";
-  for(auto ssi = m_sessionWaitForRemote.begin(); ssi != m_sessionWaitForRemote.end(); ++ssi)
+  void remote::onSessionClosed(session::shr_t ss)
   {
-    if(*ssi == ss)
+    BOOST_LOG_TRIVIAL(debug) << "error on session " << ss << ", excluding it from list";
+    for(auto ssi = m_sessionWaitForRemote.begin(); ssi != m_sessionWaitForRemote.end(); ++ssi)
     {
-      m_sessionWaitForRemote.erase(ssi);
-      BOOST_LOG_TRIVIAL(debug) << "excluded, new list size is " << m_sessionWaitForRemote.size();
-      return;
+      if(*ssi == ss)
+      {
+        m_sessionWaitForRemote.erase(ssi);
+        BOOST_LOG_TRIVIAL(debug) << "excluded, new list size is " << m_sessionWaitForRemote.size();
+        return;
+      }
     }
+    if(m_sessionH == ss) m_sessionH.reset();
+    if(m_sessionL == ss) m_sessionL.reset();
   }
-  if(m_sessionH == ss) m_sessionH.reset();
-  if(m_sessionL == ss) m_sessionL.reset();
-}
 
 
   void remote::send(uzel::Msg::shr_t msg)
-{
-  if(!connected())
   {
-      // no connection
-    m_outHighQueue.emplace_back(QueuedMsg(msg));
-  } else {
-    m_sessionH->putOutQueue(msg);
+    if(!connected())
+    {
+        // no connection
+      m_outHighQueue.emplace_back(QueuedMsg(msg));
+    } else {
+      m_sessionH->putOutQueue(msg);
+    }
   }
-}
 
 }
