8c752d711e1b17a3f109db3ea8a11b92c453becd
sena@s73.work <Sergey>
2025-08-17 19:24:29 +0200

    indent sources

--- app/userver/OutgoingManager.h
+++ app/userver/OutgoingManager.h
@@ -14,88 +14,88 @@ BETTER_ENUM(HostStatus, int8_t, initial, resolving, resolving_error, connecting,
 namespace uzel {
 
 /** Status of the remote host to connect to */
-class RemoteHostToConnect
-{
-public:
-  explicit RemoteHostToConnect(std::string hostname)
-    : m_hostname{std::move(hostname)}, m_statusChangeTS{std::chrono::steady_clock::now()}
+  class RemoteHostToConnect
   {
-  }
+  public:
+    explicit RemoteHostToConnect(std::string hostname)
+      : m_hostname{std::move(hostname)}, m_statusChangeTS{std::chrono::steady_clock::now()}
+      {
+      }
 
-  void setStatus(HostStatus hs) {
-    m_status = hs;
-    m_statusChangeTS = std::chrono::steady_clock::now();
-  }
+    void setStatus(HostStatus hs) {
+      m_status = hs;
+      m_statusChangeTS = std::chrono::steady_clock::now();
+    }
 
-  [[nodiscard]] HostStatus status() const {return m_status; }
-  [[nodiscard]] std::chrono::steady_clock::time_point statusChangeTS() const {return m_statusChangeTS; }
-  [[nodiscard]] std::string hostname() const {return m_hostname;}
+    [[nodiscard]] HostStatus status() const {return m_status; }
+    [[nodiscard]] std::chrono::steady_clock::time_point statusChangeTS() const {return m_statusChangeTS; }
+    [[nodiscard]] std::string hostname() const {return m_hostname;}
 
-  void setAddr(const boost::asio::ip::address &addr) {
-    m_addr=addr;
-  }
+    void setAddr(const boost::asio::ip::address &addr) {
+      m_addr=addr;
+    }
 
 
-private:
-  std::string m_hostname;
-  boost::asio::ip::address m_addr;
-  HostStatus m_status{HostStatus::initial};
-  std::chrono::steady_clock::time_point m_statusChangeTS;
-  size_t m_sessions{0};
-};
+  private:
+    std::string m_hostname;
+    boost::asio::ip::address m_addr;
+    HostStatus m_status{HostStatus::initial};
+    std::chrono::steady_clock::time_point m_statusChangeTS;
+    size_t m_sessions{0};
+  };
 
 
 /** Maintain outgoing connections to remote hosts (no listen/accept here) */
-class OutgoingManager final
-{
-public:
-  explicit OutgoingManager(
-    NetAppContext::shr_t,
-    const uzel::IpToSession &ipToSession,
-    const uzel::NodeToSession &nodeToSession);
+  class OutgoingManager final
+  {
+  public:
+    explicit OutgoingManager(
+      NetAppContext::shr_t,
+      const uzel::IpToSession &ipToSession,
+      const uzel::NodeToSession &nodeToSession);
 
-  OutgoingManager(OutgoingManager &&) = delete;
-  OutgoingManager &operator=(const OutgoingManager &) = delete;
-  OutgoingManager &operator=(OutgoingManager &&) = delete;
-  OutgoingManager(const OutgoingManager &other) = delete;
-  ~OutgoingManager() = default;
+    OutgoingManager(OutgoingManager &&) = delete;
+    OutgoingManager &operator=(const OutgoingManager &) = delete;
+    OutgoingManager &operator=(OutgoingManager &&) = delete;
+    OutgoingManager(const OutgoingManager &other) = delete;
+    ~OutgoingManager() = default;
 
-  using UMap = std::unordered_map<std::string, RemoteHostToConnect>;
+    using UMap = std::unordered_map<std::string, RemoteHostToConnect>;
 
-  void startConnecting(const std::string &hname);
+    void startConnecting(const std::string &hname);
 
-    /** call to start outgoing connections */
-  void start();
+      /** call to start outgoing connections */
+    void start();
 
-    /**
-     * Pull the next tick forward to "now".
-     * Call this from anywhere (any thread) when a connection just closed
-     **/
-  void poke();
+      /**
+       * Pull the next tick forward to "now".
+       * Call this from anywhere (any thread) when a connection just closed
+       **/
+    void poke();
 
-    // NOLINTBEGIN(misc-non-private-member-variables-in-classes,cppcoreguidelines-non-private-member-variables-in-classes)
-  boost::signals2::signal<void (uzel::session::shr_t unauth)> s_sessionCreated;
-    // NOLINTEND(misc-non-private-member-variables-in-classes,cppcoreguidelines-non-private-member-variables-in-classes)
+      // NOLINTBEGIN(misc-non-private-member-variables-in-classes,cppcoreguidelines-non-private-member-variables-in-classes)
+    boost::signals2::signal<void (uzel::session::shr_t unauth)> s_sessionCreated;
+      // NOLINTEND(misc-non-private-member-variables-in-classes,cppcoreguidelines-non-private-member-variables-in-classes)
 
 
-private:
-  void scheduleNext(int seconds);
-  void startConnecting();
-  void startResolving(RemoteHostToConnect &rh);
-  void connectResolved(const boost::system::error_code ec, const boost::asio::ip::tcp::resolver::results_type rezit, RemoteHostToConnect &rh);
+  private:
+    void scheduleNext(int seconds);
+    void startConnecting();
+    void startResolving(RemoteHostToConnect &rh);
+    void connectResolved(const boost::system::error_code ec, const boost::asio::ip::tcp::resolver::results_type rezit, RemoteHostToConnect &rh);
 
 
 // continue private section: variable members
-  NetAppContext::shr_t m_netctx;
-  boost::asio::strand<boost::asio::any_io_executor> m_strand;
-  boost::asio::steady_timer m_timer;
-  const uzel::IpToSession &m_ipToSession;
-  const uzel::NodeToSession &m_nodeToSession;
-  UMap m_connectTo; ///!< map remote hostnames to be connected to their status
-  const int RefreshHostStatus_sec{15};
-  const int DelayReconnect_sec{30};
-  const int ActionTimeout_sec{15};
-};
+    NetAppContext::shr_t m_netctx;
+    boost::asio::strand<boost::asio::any_io_executor> m_strand;
+    boost::asio::steady_timer m_timer;
+    const uzel::IpToSession &m_ipToSession;
+    const uzel::NodeToSession &m_nodeToSession;
+    UMap m_connectTo; ///!< map remote hostnames to be connected to their status
+    const int RefreshHostStatus_sec{15};
+    const int DelayReconnect_sec{30};
+    const int ActionTimeout_sec{15};
+  };
 
 
 }
--- app/userver/remote.h
+++ app/userver/remote.h
@@ -9,37 +9,37 @@ namespace uzel
 {
 
 /** incapsulates logical channel to one remote */
-class remote
-{
-public:
-  explicit remote(NetAppContext::shr_t netctx, std::string nodename);
+  class remote
+  {
+  public:
+    explicit remote(NetAppContext::shr_t netctx, std::string nodename);
 
-  remote &operator=(const remote &) = delete;
-  remote(const remote &other) = delete;
-  remote(remote &&) = default;
-  remote &operator=(remote &&) = delete;
-  ~remote() = default;
+    remote &operator=(const remote &) = delete;
+    remote(const remote &other) = delete;
+    remote(remote &&) = default;
+    remote &operator=(remote &&) = delete;
+    ~remote() = default;
 
-  void addSession(session::shr_t);
-  void send(uzel::Msg::shr_t msg);
-  [[nodiscard]] bool connected() const;
-  void handlePriorityMsg(const uzel::Msg &msg);
-private:
-  explicit remote(std::string &&nodeName);
+    void addSession(session::shr_t);
+    void send(uzel::Msg::shr_t msg);
+    [[nodiscard]] bool connected() const;
+    void handlePriorityMsg(const uzel::Msg &msg);
+  private:
+    explicit remote(std::string &&nodeName);
 
-  void onSessionClosed(session::shr_t ss);
+    void onSessionClosed(session::shr_t ss);
 
-  std::string m_node; ///< remote node name - known only after authentication
-  std::string m_hname; ///< remote hostname (if known) that was used to connect to
-  std::string m_addr; ///< last known remote address
-  std::unordered_set<session::shr_t> m_sessionWaitForRemote; ///!< keeps list of tcp sessions
-  session::shr_t m_sessionH; ///< high priority session
-  session::shr_t m_sessionL; ///< low priority session
-  session::shr_t m_sessionC; ///< control session (always outgoing), if low and high are incoming
-  MsgQueue m_outHighQueue; ///< high prirority outgoing message queue
-  MsgQueue m_outLowQueue; ///< low priority outgoing message queue
-  NetAppContext::shr_t m_netctx;
-};
+    std::string m_node; ///< remote node name - known only after authentication
+    std::string m_hname; ///< remote hostname (if known) that was used to connect to
+    std::string m_addr; ///< last known remote address
+    std::unordered_set<session::shr_t> m_sessionWaitForRemote; ///!< keeps list of tcp sessions
+    session::shr_t m_sessionH; ///< high priority session
+    session::shr_t m_sessionL; ///< low priority session
+    session::shr_t m_sessionC; ///< control session (always outgoing), if low and high are incoming
+    MsgQueue m_outHighQueue; ///< high prirority outgoing message queue
+    MsgQueue m_outLowQueue; ///< low priority outgoing message queue
+    NetAppContext::shr_t m_netctx;
+  };
 
   using NodeToSession = std::unordered_map<std::string, remote>;
 }
